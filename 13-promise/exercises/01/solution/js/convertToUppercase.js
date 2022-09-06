b72a06c5d88752e5aef6ab2d39436bbc2cc480d4
annivadas <anni.vadas@gmail.com>
2022-09-06 14:24:20 +0200

    done

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,26 @@
+/*
+1. Írj egy függvényt, `convertToUppercase()` néven, mely paraméterként kap egy tömböt. A függvény visszatérési érték egy Promise.  
+Amennyiben a tömb minden eleme string, és sikeresen nagybetűssé alakíthatók a Promise resolved lesz és visszaad egy új tömböt, ami az eredeti tömb összes elemét nagybetűssé alakítva tartalmazza.  
+Ellenkező esetben a Promise rejected lesz, a visszatérési érték a következő string: 'Error: Not all items in the array are strings!'
+*/
+
+const convertToUppercase = (arr = ['a', 'b', 'c']) => {
+  return new Promise((resolve, reject) => {
+    let upperCaseArr = [];
+    try {
+      arr.forEach((el) => {
+        upperCaseArr.push(el.toUpperCase());
+      });
+
+      resolve(upperCaseArr);
+
+    } catch (error) {  
+      reject(new Error('Error: Not all items in the array are strings!'));
+    }
+  });
+};
+
+
+
+
 export default convertToUppercase;
