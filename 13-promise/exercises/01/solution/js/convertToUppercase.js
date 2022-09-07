49e21ccbd03cee930044bb18efdea68d0476f0b8
annivadas <anni.vadas@gmail.com>
2022-09-07 14:35:52 +0200

    done

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -3,8 +3,20 @@
 Amennyiben a tömb minden eleme string, és sikeresen nagybetűssé alakíthatók a Promise resolved lesz és visszaad egy új tömböt, ami az eredeti tömb összes elemét nagybetűssé alakítva tartalmazza.  
 Ellenkező esetben a Promise rejected lesz, a visszatérési érték a következő string: 'Error: Not all items in the array are strings!'
 */
+const convertToUppercase = (arr=[]) => {
+  
+   return new Promise( (resolve, reject) => {
+    
+     if (arr.every( el => typeof el === 'string')) {
+      let newArray = arr.map( el => el.toUpperCase());
+      resolve(newArray);
+    } else {
+      reject(new Error('Error: Not all items in the array are strings!'));
+    }
+  });
+};
 
-const convertToUppercase = (arr = ['a', 'b', 'c']) => {
+/* const convertToUppercase = (arr = ['a', 'b', 'c']) => {
   return new Promise((resolve, reject) => {
     let upperCaseArr = [];
     try {
@@ -18,7 +30,18 @@ const convertToUppercase = (arr = ['a', 'b', 'c']) => {
       reject(new Error('Error: Not all items in the array are strings!'));
     }
   });
-};
+}; */
+
+/* const convertToUppercase = (array) => {
+  return new Promise((resolve, reject) => {
+    if (array.every(element => typeof element === 'string')) {
+      let newArray = array.map(element => element.toUpperCase());
+      resolve(newArray);
+    } else {
+      reject(new Error('Error: Not all items in the array are strings!'));
+    }
+  })
+} */
 
 
 
