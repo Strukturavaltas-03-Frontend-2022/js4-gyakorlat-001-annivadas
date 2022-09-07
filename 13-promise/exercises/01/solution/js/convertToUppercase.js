ebb91834c4e21404acb8f8386466eeb8c07e7fdc
annivadas <anni.vadas@gmail.com>
2022-09-07 14:40:05 +0200

    done

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -16,32 +16,9 @@ const convertToUppercase = (arr=[]) => {
   });
 };
 
-/* const convertToUppercase = (arr = ['a', 'b', 'c']) => {
-  return new Promise((resolve, reject) => {
-    let upperCaseArr = [];
-    try {
-      arr.forEach((el) => {
-        upperCaseArr.push(el.toUpperCase());
-      });
 
-      resolve(upperCaseArr);
 
-    } catch (error) {  
-      reject(new Error('Error: Not all items in the array are strings!'));
-    }
-  });
-}; */
 
-/* const convertToUppercase = (array) => {
-  return new Promise((resolve, reject) => {
-    if (array.every(element => typeof element === 'string')) {
-      let newArray = array.map(element => element.toUpperCase());
-      resolve(newArray);
-    } else {
-      reject(new Error('Error: Not all items in the array are strings!'));
-    }
-  })
-} */
 
 
 
--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -23,23 +23,6 @@ const sortStrings = (arr=['third', 'second', 'first']) => {
 };
 
 
-/*
-const sortStrings = (array) => {
-    return new Promise((resolve, reject) => {
-        try {
-            if (array.every(element => typeof element === 'string')) {
-                let newArray = array.sort();
-                resolve(newArray);
-            } else {
-                reject(new Error('Error: Not all items in the array are strings!'));
-            }
-        } catch (error) {
-            reject(new Error('Error: Something went wrong with sorting words!'))
-        }
-    })
-}
-
- */
 
 
 export default sortStrings;
